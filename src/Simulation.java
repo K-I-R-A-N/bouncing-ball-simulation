21a96c371079132ed3bbc4b3b67547f0a7b63c04
Nikunj Paliwal <iamnikunjp@gmail.com>
2015-01-07 18:30:19 +0000

    Created basic framework

---
+++ src/Simulation.java
@@ -0,0 +1,112 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+
+public class Simulation extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final double TICK_RATE = 60;
+
+	private static final Font FONT = new Font("Trebuchet MS", Font.BOLD, 50);
+
+	public static boolean isRunning = false;
+	public static boolean debugMode = false;
+
+	private Thread thread;
+	Window window;
+
+	public Simulation(Window w) {
+		window = w;
+	}
+
+	private void render() {
+		BufferStrategy bufferStrategy = getBufferStrategy();
+
+		if (bufferStrategy == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+
+		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+
+		g.setFont(FONT);
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		// Draw stuff here!
+
+		if (debugMode) {
+			// Debug info here!
+		}
+
+		g.dispose();
+		bufferStrategy.show();
+	}
+
+	private void tick() {
+		
+	}
+
+	@Override
+	public void run() {
+		long previousTime = System.nanoTime();
+		double nanoSeconds = 1000000000 / TICK_RATE;
+		double timeElapsed = 0;
+
+		long gameTimer = System.currentTimeMillis();
+
+		double ticks = 0;
+		double fps = 0;
+
+		while (isRunning) {
+			long currentTime = System.nanoTime();
+			timeElapsed += (currentTime - previousTime) / nanoSeconds;
+			previousTime = currentTime;
+
+			while (timeElapsed >= 1) {
+				tick();
+				ticks++;
+				timeElapsed--;
+			}
+
+			render();
+			fps++;
+
+			if (System.currentTimeMillis() - gameTimer > 1000) {
+				gameTimer += 1000;
+
+				if (debugMode) {
+					System.out.println("FPS: " + (int) fps + " TICKS: "
+							+ (int) ticks);
+					window.setTitle(Window.TITLE + " | FPS: " + (int) fps
+							+ " TICKS: " + (int) ticks);
+				} else {
+					window.setTitle(Window.TITLE);
+				}
+
+				fps = 0;
+				ticks = 0;
+			}
+		}
+		stopGame();
+	}
+
+	public synchronized void startGame() {
+		if (isRunning)
+			return;
+		isRunning = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public synchronized void stopGame() {
+		if (!isRunning)
+			return;
+		isRunning = false;
+		thread = null;
+		System.exit(0);
+	}
+}
---
+++ src/Window.java
@@ -0,0 +1,28 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final double WIDTH = 1280;
+	public static final double HEIGHT = 720;
+	public static final String TITLE = "Bouncing Ball Simulation";
+	
+	public Window() {
+		Simulation game = new Simulation(this);
+		game.setPreferredSize(new Dimension((int) WIDTH, (int) HEIGHT));
+		game.setMinimumSize(new Dimension((int) WIDTH, (int) HEIGHT));
+		game.setMaximumSize(new Dimension((int) WIDTH, (int) HEIGHT));
+		add(game);
+		pack();
+		game.requestFocus();
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle(TITLE);
+		setVisible(true);
+		game.startGame();
+	}
+}
